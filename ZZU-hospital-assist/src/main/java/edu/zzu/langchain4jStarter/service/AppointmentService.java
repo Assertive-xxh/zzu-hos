d11f983cbb8203bb2bcdf2e9cd27f987116ef58e
yanghao <15560393+zzu_yh@user.noreply.gitee.com>
2025-09-21 03:42:29 +0000

    删除文件 ZZU-hospital-assist

--- ZZU-hospital-assist/src/main/java/edu/zzu/langchain4jStarter/service/AppointmentService.java
+++
@@ -1,9 +0,0 @@
-package edu.zzu.langchain4jStarter.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import edu.zzu.langchain4jStarter.entity.AppointmentRecord;
-
-public interface AppointmentService extends IService<AppointmentRecord> {
-
-    AppointmentRecord getOne(AppointmentRecord record);
-}
